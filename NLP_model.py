85c715531a03cbc5c128f797fbb58b2e26fd0ae8
KartikeyBartwal <bartwalkartikey1@gmail.com>
2024-02-10 10:32:59 +0530

    all files added

---
+++ NLP_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fc0de83be62796ded3f08f69e90ec74852aa3c408e905f8685b21037e84478fb
+size 6993
---
+++ dockerfile
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8c2d3d6086bcd42278af20dbc15b3ee19f72818d490db514aefddbaac879f15e
+size 418
